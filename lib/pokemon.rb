a0e1b527002c75e32017816369c0bd99986bd5d1
Emily Asch <e.asch@me.com>
2018-12-02 19:47:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,26 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def initialize (keyword)
+    # @id = id 
+    # @name = name 
+    # @type = type 
+    # @db = db
+    
+    @@all << self 
+    end 
+  
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon(name, type) VALUES (?,?)", name, type)
+  end 
+  
+  def self.find(id, db)
+    db.execute("SELECT pokemon.id FROM pokemon WHERE id = ")
+  end 
+    
 end
